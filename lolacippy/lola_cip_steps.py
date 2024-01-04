a25692295fe2d88bb75c8bacf39cb66328ac1b42
Jorge Trujillo Perez <jorgeluistrujilllo@gmail.com>
2024-01-04 11:02:45 -0500

    singleton to CIP steps

--- lolacippy/lola_cip_steps.py
+++ lolacippy/lola_cip_steps.py
@@ -11,42 +11,57 @@ from .cip.cip_Utils import CipUtils
 
 
 class LolaCipSteps:
+    _instance = None
+
     
-    
-    def __init__(self, lola_kraken: LolaKrakenServicesManager, lola_sdk: LolaSDK, config):
-        self.lola_kraken_services_manager = lola_kraken
-        self.lola_sdk = lola_sdk
-        self.events_onImageMessage = OnImageMessage(lola_kraken,config)
-        self.notifications = OnNotification(lola_kraken,config)
-        self.lola_messages = MessagesConfig(config) 
-        self.newConversation = OnNewConversation(lola_kraken,config)
-        self.events_onCommand = OnCommand(lola_kraken,config)
-        self.lola_cip_utils = CipUtils(config)
-        self.config = config
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(LolaCipSteps, cls).__new__(cls)
+            cls._instance.lola_kraken = kwargs.get('lola_kraken')
+            cls._instance.lola_sdk = kwargs.get('lola_sdk')
+            cls._instance.config = kwargs.get('config')
+            cls._instance.events_onImageMessage = OnImageMessage(cls._instance.lola_kraken,cls._instance.config)
+            cls._instance.notifications = OnNotification(cls._instance.lola_kraken,cls._instance.config)
+            cls._instance.lola_messages = MessagesConfig(cls._instance.config) 
+            cls._instance.newConversation = OnNewConversation(cls._instance.lola_kraken,cls._instance.config)
+            cls._instance.events_onCommand = OnCommand(cls._instance.lola_kraken,cls._instance.config)
+            cls._instance.lola_cip_utils = CipUtils(cls._instance.config)
+        return cls._instance
+           
         
-    def initAssitantCip(self,session,ctx:LolaContext):
-        self.newConversation.initAssitant(session=session,ctx=ctx)
-        allowed_documents = self.lola_cip_utils.transformDocumentCountryText()
-        bank_name = self.config.get("bank_name")
-        welcome_message = self.lola_messages.getWelcomeMessage().format(bank_name=bank_name)
+        
+    @classmethod
+    def getInstance(cls, lola_kraken: LolaKrakenServicesManager=None, lola_sdk: LolaSDK=None, config=None):
+        if cls._instance is None:
+            cls._instance = LolaCipSteps(lola_kraken, lola_sdk, config)
+        return cls._instance
+    @classmethod  
+    def initAssitantCip(cls,session,ctx:LolaContext):
+        cls._instance.newConversation.initAssitant(session=session,ctx=ctx)
+        allowed_documents = cls._instance.lola_cip_utils.transformDocumentCountryText()
+        bank_name = cls._instance.config.get("bank_name")
+        welcome_message = cls._instance.lola_messages.getWelcomeMessage().format(bank_name=bank_name)
         returnMessage = welcome_message + "\n".join(allowed_documents)
         return returnMessage
         
         
-        
-    def scanId(self,session,ctx:LolaContext,Url:str,msg):
-        resulScanId = self.events_onImageMessage.onboardingScanId(session=session,ctx=ctx,url=Url)
+    @classmethod
+    def scanId(cls,session,ctx:LolaContext,Url:str,msg):
+        resulScanId = cls._instance.events_onImageMessage.onboardingScanId(session=session,ctx=ctx,url=Url)
         return resulScanId
-    def selfie(self,session,ctx:LolaContext,Url:str,msg):
-        resultSelfie = self.events_onImageMessage.onboardingScanSelfie(session=session,ctx=ctx,url=Url)
-        completeCipMessage = self.lola_messages.getCompleteCipMessage()
-        resultProccesSelfie = {
-            "resultSelfie" : resultSelfie,
+    @classmethod
+    def selfie(cls,session,ctx:LolaContext,Url:str,msg):
+        resultclsie = cls._instance.events_onImageMessage.onboardingScanclsie(session=session,ctx=ctx,url=Url)
+        completeCipMessage = cls._instance.lola_messages.getCompleteCipMessage()
+        resultProccesclsie = {
+            "resultclsie" : resultclsie,
             "message" : completeCipMessage
         }
         
-        return resultProccesSelfie
-    def PooLife(self,session,ctx:LolaContext,Url:str,msg):
+        return resultProccesclsie
+    @classmethod
+    def PooLife(cls,session,ctx:LolaContext,Url:str,msg):
         eventsData = msg['data']['data']
         typeEvent = eventsData['type']
         if typeEvent == "response":
@@ -55,17 +70,19 @@ class LolaCipSteps:
                 iproovData = msg['data']['data']['response']
                 iproovLastFrame = iproovData['frame']
                 try:
-                    facematch = self.notifications.onProofOfLife(session,ctx,iproovLastFrame,iproovData)
+                    facematch = cls._instance.notifications.onProofOfLife(session,ctx,iproovLastFrame,iproovData)
                     messageFacematch = str(facematch["message"])
                     return messageFacematch
                     
                 except Exception as error:
                     print(error)
                     return str(error)
-    def default(self,session,ctx:LolaContext,Url:str,msg):
+    @classmethod
+    def default(cls,session,ctx:LolaContext,Url:str,msg):
         result = {
-            "message" : self.lola_messages.getImageNotValidMessage()
+            "message" : cls._instance.lola_messages.getImageNotValidMessage()
         }
-        return result   
-    def commandAddress(self,session,ctx:LolaContext,cmd):
-        return self.events_onCommand.comandValidateAddress(session=session,ctx=ctx,cmd=cmd)
+        return result 
+    @classmethod  
+    def commandAddress(cls,session,ctx:LolaContext,cmd):
+        return cls._instance.events_onCommand.comandValidateAddress(session=session,ctx=ctx,cmd=cmd)
